e7eedf1d16aaa0067e5035cdc29b8cc8d4c09e7b
Enrique Juan de Dios <juandedioz@gmail.com>
2020-09-20 14:40:26 -0500

    [Setup] Spring Boot Project

---
+++ src/main/java/com/tdd/demoapi/TddDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/demoapi/TddDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
